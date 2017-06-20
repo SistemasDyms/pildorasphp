b0145330b67466ffd34ddbb072abb146ef5c1235
sistemasDYMS <dairon9@gmail.com>
2017-06-20 18:34:57 -0430

    incluido video 67 cookies parte 5

---
+++ video67/eliminarCookies.php
@@ -0,0 +1,4 @@
+<?php
+setcookie("usuario_login","kendo",time()-1);
+header("location:index.php");
+?>
---
+++ video67/panel.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+$nombre=$_SESSION["usuario"];
+if(!isset($nombre)){
+  header("location:index.php");
+}
+
+ ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="../public/css/bootstrap.min.css">
+  <title>Panel de usuario</title>
+</head>
+<body>
+<div class="container">
+  <div class="row">
+    <div class="col-md-6">
+      <h1>Bienvenido al panel de control: <?php echo  $nombre; ?> </h1>
+      <h3> <a href="panel-img.php">Ir al panel de imagenes</a> </h3>
+    </div>
+    <div class="col-md-6">
+      <h1>Cerrar sesion</h1><a href="cerrar.php">aqui</a>
+    </div>
+  </div>
+</div>
+</body>
+</html>
