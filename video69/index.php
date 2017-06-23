9009f753dcfb7cd186646e45a43bec886f3c089c
sistemasDYMS <dairon9@gmail.com>
2017-06-22 22:40:18 -0430

    incluido video69

---
+++ video69/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Video 69</title>
+	<link rel="stylesheet" href="../public/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+					<h1>encriptando password</h1>
+					<h3>consulta de usuarios</h3>
+					<form class="form-horizontal" action="consulta.php" method="post">
+						<div class="form-group">
+							<label for="user">Usuario</label>
+							<input type="text" class="form-control" name="user" id="user">
+						</div>
+						<div class="form-group">
+							<label for="pass">password</label>
+							<input type="password" name="pass" class="form-control" id="pass">
+						</div>
+						<input type="submit" name="Enviar" value="Registrarse">
+					</form>
+
+			</div>
+		</div>
+	</div>
+</body>
+</html>
