96ee66c28b095b132a287fc215fa2b98a2bb0c0a
sistemasDYMS <dairon9@gmail.com>
2017-06-26 17:50:40 -0430

    incluido video78 mvc explicacion parte1

---
+++ video78/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>video 78 Modelo vista controlador parte 1</title>
+</head>
+<body>
+	<p>Explicando todo lo referente con el modelo vista controlador</p>
+</body>
+</html>
