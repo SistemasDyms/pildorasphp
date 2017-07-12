d62bce7846f95493985851ce798b7ce158cee418
sistemasDYMS <dairon9@gmail.com>
2017-07-12 18:07:53 -0430

    incluido video91 creacion de un blog parte 3

---
+++ video91/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>video 91 creacion de un BLOG parte3</title>
+	<link rel="stylesheet" type="text/css" href="../public/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+		<div class="row">
+			<div class="col-md-2">
+			<h1 class="text-center">Video91</h1>
+			<h6>Blog</h6>
+			</div>
+			<div class="col-md-10">
+				<h2>Creacion de un Blog parte 3</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+		<br>
+		<br>
+		<br>
+		<br>
+		<form action="insertar.php" method="POST" enctype="multipart/form-data"->
+			<div class="form-group">
+				<label for="titulo">Titulo del Articulo</label>
+				<input type="text" name="titulo" id="titulo" class="form-control">	
+			</div>
+			<div class="form-group">
+				<label for="articulo">Artículo</label>
+				<textarea rows="10" cols="76" id="articulo" name="articulo"></textarea>	
+			</div>
+			<input type="hidden" name="MAX_IMG" value="2097152">
+			<div class="form-group">
+				<label for="img">Imagen</label>
+				<input type="file" name="img" id="img" class="form-control">	
+			</div>
+			<input type="submit" name="enviar" class="btn btn-success btn-lg" Value="Enviar Artículo" >
+
+		</form>	
+		</div>
+		</div>
+	</div>
+</body>
+</html>
